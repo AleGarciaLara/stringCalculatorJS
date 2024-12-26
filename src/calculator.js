d941b44fc3dbf2def1e9834def59c9ada6011ff1
Alejandra García <alejandra.garcialara@gmail.com>
2024-12-26 14:20:59 -0600

    Added support for custom delimiters.

--- src/calculator.js
+++ src/calculator.js
@@ -4,6 +4,14 @@ const calculator = {
     if (text === '') {
       return 0;
     }
+    // Add support for custom delimiter
+    if (text.startsWith('//')) {
+      const delimiter = text[2];
+      const numbersString = text.slice(4);
+      const numbers = numbersString.split(delimiter).map(Number);
+      return numbers.reduce((acc, curr) => acc + curr, 0);
+
+    }
     // Tasks simplified
     const numbers = text.split('\n').join(',').split(',').map(Number);
     return numbers.reduce((acc, curr) => acc + curr, 0);
